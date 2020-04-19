6765575e78faa13aa4eeda5acc7573208377d6db
piyush shrivastava <piyush@leena.ai | piyush.srivstatavaimraw@gmail.com>
2020-04-19 21:10:43 +0530

    removed debugger statement

--- examples/src/index.js
+++ examples/src/index.js
@@ -4,7 +4,7 @@ const rootDom = document.getElementById('root')
 
 function tick(elements = [1, 2]) {
   const time = new Date().toLocaleTimeString()
-  const clockElement = <div>{elements.map(item => item)}</div>
+  const clockElement = <div>{elements.map(item => `${time} `)}</div>
   SimplReact.render(clockElement, rootDom)
 }
 
--- src/simplreact.ts
+++ src/simplreact.ts
@@ -1,13 +1,11 @@
 import element, { Instance } from './types/element'
 import { createElement } from './lib/CreateElement'
-import { instantiate } from './lib/Instantiate'
 import { Reconcile } from './lib/Reconcile'
 
 export default class SimplReact {
   static createElement = createElement
   private static rootInstance: Instance | null = null
   static render(element: element, parentDom: HTMLElement | Text) {
-    debugger
     const prevInstance = this.rootInstance
     const nextInstance: Instance | null = Reconcile(parentDom, prevInstance, element)
     this.rootInstance = nextInstance
